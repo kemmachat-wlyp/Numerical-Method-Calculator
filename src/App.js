0b831952978e980e2172f38e6375cf21a4f57141
Kemmachat Wanlayaphorn <70964204+kemmachat-wlyp@users.noreply.github.com>
2021-05-23 18:37:17 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -121,18 +121,6 @@ class App extends Component {
                 </SubMenu>
 
                 <SubMenu
-                  key="sub3"
-                  title={
-                    <span>
-                      <Icon type="setting" />
-                      <span>Regression</span>
-                    </span>
-                  }
-                >                  
-                  <Menu.Item key="menu_cramer">Linear Regression<Link to="/Linear_Regression" /> </Menu.Item>
-                </SubMenu>
-
-                <SubMenu
                   key="sub4"
                   title={
                     <span>
